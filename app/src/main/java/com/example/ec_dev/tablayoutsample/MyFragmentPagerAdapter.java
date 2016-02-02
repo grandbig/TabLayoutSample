63755abeffddaf7828eb69524d7abdb5f455d851
ec_dev <takahiro.kato@g.softbank.co.jp>
2016-02-02 10:39:58 +0900

    MainActivity内のFragment処理を分離

---
+++ app/src/main/java/com/example/ec_dev/tablayoutsample/MyFragmentPagerAdapter.java
@@ -0,0 +1,33 @@
+package com.example.ec_dev.tablayoutsample;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by ec_dev on 2016/02/02.
+ */
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    final private String[] pageTitles = {"HOME", "SEARCH", "SETTINGS"};
+
+    // コンストラクタ
+    public MyFragmentPagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PageFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return pageTitles[position];
+    }
+
+    @Override
+    public int getCount() {
+        return pageTitles.length;
+    }
+}
